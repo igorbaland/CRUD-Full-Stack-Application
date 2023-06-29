080dc8b44e588b89fd7b1f84f3f90b299850c72d
Igor <igor.balandin@gmx.de>
2023-06-29 08:36:24 +0200

    Added Cross Origin.
    - Acces control, allow Origin localhost:4200, beispiel.de...
    - without -> response blocked
    
    	modified:   springboot.backend/src/main/java/com/bal/springboot/backend/controller/EmployeeController.java

--- springboot.backend/src/main/java/com/bal/springboot/backend/controller/EmployeeController.java
+++ springboot.backend/src/main/java/com/bal/springboot/backend/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.bal.springboot.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bal.springboot.backend.model.Employee;
 import com.bal.springboot.backend.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/v1/")
 public class EmployeeController {
