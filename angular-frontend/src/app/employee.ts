2f7fa012ddbae21a936790b9a34aee34f4519f1c
Igor <igor.balandin@gmx.de>
2023-06-29 08:55:36 +0200

    improved overview

--- angular-frontend/src/app/employee.ts
+++ angular-frontend/src/app/employee.ts
@@ -1,7 +1,7 @@
 export class Employee {
     id: number;
     firstName: string;
-    lastName:  string;
+    lastName: string;
     emailId: string;
 
     constructor(id: number,
@@ -9,10 +9,9 @@ export class Employee {
                 lastName: string,
                 emailId: string){
 
-                    
         this.id = id;
         this.firstName = firstName;
-        this.lastName =lastName;
+        this.lastName = lastName;
         this.emailId = emailId;
     }
 }
