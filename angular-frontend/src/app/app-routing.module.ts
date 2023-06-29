34cc44ffbcc9d64ea99a0198cc96877772147564
Igor <igor.balandin@gmx.de>
2023-06-29 16:50:06 +0200

    created router for component create-employee

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 const routes: Routes = [
   {path:'employees', component: EmployeeListComponent},
-  {path: '', redirectTo: 'employees', pathMatch: 'full'}
-
+  {path: '', redirectTo: 'employees', pathMatch: 'full'},
+  {path: 'create-employee', component: CreateEmployeeComponent}
 ];
 
 @NgModule({
