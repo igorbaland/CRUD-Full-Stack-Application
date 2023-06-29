c9abf8248b666025adc7df4ddbfb1ea1059c3d49
Igor <igor.balandin@gmx.de>
2023-06-29 17:11:42 +0200

    Removed args from constructor

--- angular-frontend/src/app/employee.ts
+++ angular-frontend/src/app/employee.ts
@@ -1,17 +1,19 @@
 export class Employee {
-    id: number;
-    firstName: string;
-    lastName: string;
-    emailId: string;
+    id!: number;
+    firstName!: string;
+    lastName!: string;
+    emailId!: string;
 
-    constructor(id: number,
+    constructor(){
+
+       /* id: number,
                 firstName: string,
                 lastName: string,
-                emailId: string){
+                emailId: string
 
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.emailId = emailId;
+        this.emailId = emailId;*/
     }
 }
