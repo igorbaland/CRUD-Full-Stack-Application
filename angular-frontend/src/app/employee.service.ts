9d81e6ec84bf5aceb1e94c4ec8404362c7e2cd77
Igor <igor.balandin@gmx.de>
2023-07-01 06:32:17 +0200

    Added service getUserById

--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -20,4 +20,10 @@ export class EmployeeService {
     return this.httpClient.post(`${this.baseURL}`, employee);
   }
 
+  getEmployeeById(id: number): Observable<Employee>{
+    return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
+  }
+
+ 
+
 }
